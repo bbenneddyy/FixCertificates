75e54ade2b66345681fb2cb520b2915db3d62f4f
Akukinaqua <guynachanok2@gmail.com>
2024-06-10 23:15:48 +0700

    add choice มอต้น

--- src/components/RegisterationForm/RegisterationFrom.tsx
+++ src/components/RegisterationForm/RegisterationFrom.tsx
@@ -11,6 +11,7 @@ function RegisterationForm() {
           className="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-e-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 max-w-2xl"
         >
           <option>- โปรดระบุระดับชั้นการศึกษา -</option>
+          <option>- มัธยมศึกษาตอนต้น -</option>
           <option>- มัธยมศึกษาปีที่4 -</option>
           <option>- มัธยมศึกษาปีที่5 -</option>
           <option>- มัธยมศึกษาปีที่6 -</option>
