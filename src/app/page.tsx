6e93fecd9bdd9e52f3b493d09ce2fded650dfbff
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-06-11 17:07:41 +0700

    feat: update home page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,57 @@
+import Link from "next/link";
 import Navbar from "../components/Navbar/Navbar";
-import Image from "next/image";
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="relative h-screen">
-        <Image
-          src="/images/placeholder.jpg"
-          alt="placeholder"
-          layout="fill"
-          objectFit="cover"
-        />
-
-        <Image
-          src="/images/placeholder2.jpg"
-          alt="placeholder2"
-          width={100}
-          height={200}
-          className="absolute top-16 left-8 rounded-3xl w-1/2 hover:brightness-75 transition-all duration-500"
-        />
-        <div
-          style={{ top: "480px" ,width: "546.5px"}}
-          className="absolute top-64 left-16 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center justify-center transform hover:scale-110 transition-transform duration-500"
-        >
-          button
+      <div className="bg-white">
+        <div className="relative isolate px-6 pt-14 lg:px-8">
+          <div
+            className="absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-20"
+            aria-hidden="true"
+          >
+            {/* green background */}
+            <div
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#16a34a] to-[#22c55e] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+            />
+          </div>
+          <div className="mx-auto max-w-3xl py-32 sm:py-48 lg:py-56">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                <Link href="https://www.facebook.com/MDCUmedcamp" rel="noopener noreferrer" target="_blank" className="font-semibold text-green-600">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  อ่านเพิ่มเติม <span aria-hidden="true">&rarr;</span>
+                </Link>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                เสวนาค่ายอยากเป็นหมอ
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+               กิจกรรมเสวนาจากรุ่นพี่คณะแพทยศาสตร์ จุฬาลงกรณ์มหาวิทยาลัยให้นักเรียนมัธยมศึกษาและผู้ปกครอง
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <Link href="/register" className="rounded-md bg-green-600 px-6 py-2.5 text-xl font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">สมัคร</Link>
+              </div>
+            </div>
+          </div>
+          <div
+            className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#16a34a] to-[#22c55e]  opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
-//import logo from "../public/chulalogo.jpg";
 
 export default function Navbar() {
   return (
-    <header className="flex items-center px-8 py-10 space-x-6 bg-lime-600">
-      <Image src="/images/chulalogo.png" alt="Chula Logo" width={100} height={200}/>
-      <h1 className="font-bold text-2xl">เสวนาเปิดรั้วหมอจุฬาฯ ครั้งที่ 34</h1>
+    <header className="flex items-center px-8 space-x-6 bg-gray-100">
+      <Image src="/images/chulalogo.png" alt="Chula Logo" width={100} height={150} />
+      <h1 className="font-bold text-2xl hidden md:block">เสวนาเปิดรั้วหมอจุฬาฯ ครั้งที่ 34</h1>
       <ul className="flex text-center space-x-6">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" className="border-4 p-2 rounded-lg bg-gray-200 hover:border-slate-300 transition ease-in-out">หน้าหลัก</Link>
         </li>
         <li>
-          <Link href="/register">Register</Link>
+          <Link href="/register" className="border-4 p-2 rounded-lg bg-gray-200 hover:border-slate-300 transition ease-in-out">สมัคร</Link>
         </li>
       </ul>
     </header>
