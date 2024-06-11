f6fc53e451cb5a2ff588b3fdbaae2d456d01c999
BBenneddyy <kanazaa2@gmail.com>
2024-06-11 16:00:22 +0700

    style: nav bar and home page
    practicing adding picture and button animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import Navbar from "../components/Navbar/Navbar";
-
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="relative h-screen">
+        <Image
+          src="/images/placeholder.jpg"
+          alt="placeholder"
+          layout="fill"
+          objectFit="cover"
+        />
+
+        <Image
+          src="/images/placeholder2.jpg"
+          alt="placeholder2"
+          width={100}
+          height={200}
+          className="absolute top-16 left-8 rounded-3xl w-1/2 hover:brightness-75 transition-all duration-500"
+        />
+        <div
+          style={{ top: "480px" ,width: "546.5px"}}
+          className="absolute top-64 left-16 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center justify-center transform hover:scale-110 transition-transform duration-500"
+        >
+          button
+        </div>
+      </div>
     </>
   );
 }
-
-
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
+//import logo from "../public/chulalogo.jpg";
 
 export default function Navbar() {
   return (
-    <header className="flex items-center px-8 py-10 space-x-16">
+    <header className="flex items-center px-8 py-10 space-x-6 bg-lime-600">
+      <Image src="/images/chulalogo.png" alt="Chula Logo" width={100} height={200}/>
       <h1 className="font-bold text-2xl">เสวนาเปิดรั้วหมอจุฬาฯ ครั้งที่ 34</h1>
       <ul className="flex text-center space-x-6">
         <li>
