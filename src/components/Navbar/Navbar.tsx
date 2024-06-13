0bbdec90b115c1aebc8a5d39662549506e93c9f8
Tatazaza822 <81914762+Tatazaza822@users.noreply.github.com>
2024-06-13 15:00:54 +0700

    fix cant click button
    fix button so we can click button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <header className="flex items-center px-8 space-x-6 bg-gray-100">
-      <Image src="/images/chulalogo.png" alt="Chula Logo" width={100} height={150} />
+    <header className="flex items-center px-8 space-x-6 bg-gray-100 z-20">
+      <Image src="/images/chulalogo.png" alt="Chula Logo" width={100} height= {150} />
       <h1 className="font-bold text-2xl hidden md:block">เสวนาเปิดรั้วหมอจุฬาฯ ครั้งที่ 34</h1>
       <ul className="flex text-center space-x-6">
         <li>
-          <Link href="/" className="border-4 p-2 rounded-lg bg-gray-200 hover:border-slate-300 transition ease-in-out">หน้าหลัก</Link>
+          <Link href="/" className="border-4 p-2 rounded-lg bg-gray-200 hover:border-slate-300 transition ease-in-out z-10">หน้าหลัก</Link>
         </li>
         <li>
-          <Link href="/register" className="border-4 p-2 rounded-lg bg-gray-200 hover:border-slate-300 transition ease-in-out">สมัคร</Link>
+          <Link href="/register" className="border-4 p-2 rounded-lg bg-gray-200 hover:border-slate-300 transition ease-in-out z-10">สมัคร</Link>
         </li>
       </ul>
     </header>
