04c8411e80b887fd956e96b3b9221ddb79f2ae9f
Ittipat <39715559+ittipatken@users.noreply.github.com>
2024-06-15 20:47:41 +0700

    fix: git conflict (#1)
    * fix: git conflict
    
    * fix: reset migration
    
    * fix: remove dev.db

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,18 +14,6 @@ export default function Navbar() {
           <Link href="/register" className="border-4 p-2 rounded-lg bg-gray-200 hover:border-slate-300 transition ease-in-out z-10">สมัคร</Link>
         </li>
       </ul>
-      <div>
-        <Link href="/admin123" className="border-4 border-rose-700 p-2 rounded-lg bg-red-600 hover:border-slate-300 transition ease-in-out font-bold text-gray-50">ADMIN ONLY</Link>
-      </div>
-      <div className="flex-grow"></div>{" "}
-      <div className="ml-auto">
-        <Link
-          href="/admin123"
-          className="border-4 border-rose-700 p-2 rounded-lg bg-red-600 hover:border-slate-300 transition ease-in-out font-bold text-gray-50"
-        >
-          ADMIN ONLY
-        </Link>
-      </div>
     </header>
   );
 }
