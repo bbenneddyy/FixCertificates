6b08b4b162414d501d48722389682edd0e6cc177
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-06-16 05:39:44 +0700

    refactor: remove unused codes in api route

--- src/app/api/registration/[id]/route.ts
+++ src/app/api/registration/[id]/route.ts
@@ -1,46 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import prisma from "@/utils/db";
+import { NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
-// export async function GET(
-//     req: NextApiRequest, res: NextApiResponse
-// ) {
-//     const { id } = req.query;
-//     const userRegistration = await prisma.registration.findUnique({
-//         where: {
-//           id: id as string,
-//         }});
-
-//     return res.json({
-//         registration : userRegistration
-//     });
-// }
-export async function GET(
-    request: Request,
-    {params} : {params: {id: string}}
-) {
+export async function GET({ params }: { params: { id: string } }) {
     const id = params.id;
     const userRegistration = await prisma.registration.findUnique({
         where: {
           id: id as string,
         }});
-
-    return Response.json({
+    return NextResponse.json({
         registration : userRegistration
     });
 }
-
-// export async function POST(
-//     request: Request,
-//     {params, body}: {params: {cuid: string}, body: any}
-// ) {
-//     const cuid = params.cuid;
-//     const newRegistration = body;
-
-//     const insertedRegistration = await db.insert(registrations, {...newRegistration, cuid});
-
-//     return Response.json({
-//         registration : insertedRegistration
-//     });
-// }
