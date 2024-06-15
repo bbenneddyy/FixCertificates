9fbcb45c720715106e3a313d253670bd084a4aad
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-06-15 13:10:11 +0700

    feat: update register page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,12 @@
 import type { Config } from "tailwindcss";
-import flowbite from "flowbite-react/tailwind";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {},
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
