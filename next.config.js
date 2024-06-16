78f87f3678a705d03e0a9f73c70b7481b08a7c3d
Punyawish Patumhirunruksa <87409329+Punpunyawish@users.noreply.github.com>
2024-06-16 14:54:24 +0700

    feat: next-config standalone

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  output: "standalone",
+};
+
+module.exports = nextConfig;
