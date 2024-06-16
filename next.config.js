465d0fde276a926e5f77c29c57469913d4326d5b
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-06-16 15:12:08 +0700

    perf: use new mjs config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  output: "standalone",
-};
-
-module.exports = nextConfig;
