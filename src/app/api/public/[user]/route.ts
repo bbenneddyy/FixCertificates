5743adde2af70d1139e23e0a05124701e99db2b6
Punyawish Patumhirunruksa <87409329+Punpunyawish@users.noreply.github.com>
2024-07-06 23:03:49 +0700

    fix: read file after deploy

---
+++ src/app/api/public/[user]/route.ts
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+export async function GET(request: Request, { params }: { params: { user: string } }) {
+  const user = params.user;
+  
+  if (user && user.length) {
+    const publicDir = path.join(process.cwd(), 'public/assets');
+    const fileUrl = user;
+    const filePath = path.join(publicDir, fileUrl);
+
+    try {
+      const data = await fs.readFile(filePath);
+      return new Response(data, { status: 200 });
+    } catch (error) {
+      return new Response(JSON.stringify({ error: error }), { status: 404 });
+    }
+  } else {
+    return new Response(JSON.stringify({ error: "No user" }), { status: 400 });
+  }
+}
