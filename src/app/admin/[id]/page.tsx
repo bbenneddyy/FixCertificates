fe3eff240187b496438c5613309f6b4eeb671831
ittipat <39715559+ittipatken@users.noreply.github.com>
2024-07-14 09:49:23 +0700

    feat: add loading screen to admin page

--- src/app/admin/[id]/page.tsx
+++ src/app/admin/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { db } from "@/utils/db";
-import { Suspense } from "react";
 import Image from "next/image";
 import UpdateStatusButtons from "@/components/Buttons/UpdateStatusButtons";
 
@@ -32,43 +31,41 @@ export default async function ApprovePage({ params }: { params: { id: string }; 
   }
   return (
     <div>
-      <Suspense fallback={<p>Loading...</p>}>
-        <div className="flex justify-center items-start min-h-screen bg-gray-100 pt-16">
-          <div className="w-full max-w-md m-4 p-6 bg-white rounded-lg shadow-lg border border-gray-200">
-            <div className="text-center mb-4">
-              <h1 className="text-2xl font-semibold text-gray-800">
-                {registeredUser.firstname} {registeredUser.lastname}
-              </h1>
-            </div>
-            <div className="text-gray-600 mb-4">
-              <p className="mb-2">
-                <strong>อีเมล:</strong> {registeredUser.email}
-              </p>
-              <p className="mb-2">
-                <strong>ระดับชั้น:</strong> {registeredUser.education}
-              </p>
-              <p className="mb-2">
-                <strong>สถานะ:</strong> {registeredUser.status}
-              </p>
-            </div>
-            <div className="flex justify-center">
-              {registeredUser.id &&
-                <Image
-                  src={`/api/images/${registeredUser.id}.${registeredUser.file_type}`}
-                  alt="slip"
-                  className="rounded-md shadow-md"
-                  width={250}
-                  height={250}
-                  unoptimized
-                />
-              }
-            </div>
-            <div className="flex justify-around mt-4">
-              <UpdateStatusButtons id={params.id} />
-            </div>
+      <div className="flex justify-center items-start min-h-screen bg-gray-100 pt-16">
+        <div className="w-full max-w-md m-4 p-6 bg-white rounded-lg shadow-lg border border-gray-200">
+          <div className="text-center mb-4">
+            <h1 className="text-2xl font-semibold text-gray-800">
+              {registeredUser.firstname} {registeredUser.lastname}
+            </h1>
+          </div>
+          <div className="text-gray-600 mb-4">
+            <p className="mb-2">
+              <strong>อีเมล:</strong> {registeredUser.email}
+            </p>
+            <p className="mb-2">
+              <strong>ระดับชั้น:</strong> {registeredUser.education}
+            </p>
+            <p className="mb-2">
+              <strong>สถานะ:</strong> {registeredUser.status}
+            </p>
+          </div>
+          <div className="flex justify-center">
+            {registeredUser.id &&
+              <Image
+                src={`/api/images/${registeredUser.id}.${registeredUser.file_type}`}
+                alt="slip"
+                className="rounded-md shadow-md"
+                width={250}
+                height={250}
+                unoptimized
+              />
+            }
+          </div>
+          <div className="flex justify-around mt-4">
+            <UpdateStatusButtons id={params.id} />
           </div>
         </div>
-      </Suspense>
+      </div>
     </div>
   );
 }
