ae21f83a960285a648b44bba7c5d2bc2338efb13
Akukinaqua <guynachanok2@gmail.com>
2024-08-30 23:04:34 +0700

    feat:make eventmail form

---
+++ src/utils/mail4.tsx
@@ -0,0 +1,44 @@
+import nodemailer from "nodemailer";
+import { EventOnlineEmail } from "@/components/Email/EventOnlineEmail";
+import { render } from '@react-email/render';
+
+interface ISendMail {
+  to: string;
+  subject: string;
+  firstname: string;
+  lastname: string;
+  
+}
+
+export async function sendMail4({ to, subject, firstname, lastname}: ISendMail) {
+  const { SMTP_EMAIL, SMTP_PASSWORD } = process.env;
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: SMTP_EMAIL,
+      pass: SMTP_PASSWORD,
+    },
+  });
+  try {
+    await transport.verify();
+  } catch (error) {
+    console.error("Unable to verify email ", error);
+    return;
+  }
+
+  try {
+    const emailHtml = render(<EventOnlineEmail firstname={firstname} lastname={lastname} />);
+    const result = await transport.sendMail({
+      from: `MDCU Converse <${SMTP_EMAIL}>`,
+      to,
+      subject,
+      html: emailHtml,
+    });
+    console.log("Email sent: ", result.response)
+  } catch (error) {
+    console.error("Unable to send email ", error);
+  }
+}
